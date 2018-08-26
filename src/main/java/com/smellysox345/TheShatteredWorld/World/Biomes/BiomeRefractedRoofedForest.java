5e075b44b1e23069f1cc4f58b8eada88b22d1cc6
cj3636 <phillgilmartin@gmail.com>
2018-08-26 14:17:13 -0700

    Implemented a way to easily change fog color and density on the fly. Add decorations to Roofed Forest (Vanilla shroom gens, Ores/Stones and flowers.

--- src/main/java/com/smellysox345/TheShatteredWorld/World/Biomes/BiomeRefractedRoofedForest.java
+++ src/main/java/com/smellysox345/TheShatteredWorld/World/Biomes/BiomeRefractedRoofedForest.java
@@ -3,6 +3,7 @@ package com.smellysox345.TheShatteredWorld.World.Biomes;
 import com.smellysox345.TheShatteredWorld.World.gen.generators.tree.WorldGenCanopyRefracted;
 import com.smellysox345.TheShatteredWorld.World.gen.generators.tree.WorldGenEyeTreeDefault;
 import com.smellysox345.TheShatteredWorld.util.interfaces.IBiomeMist;
+import net.minecraft.block.BlockDoublePlant;
 import net.minecraft.init.Blocks;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.MathHelper;
@@ -11,6 +12,7 @@ import net.minecraft.world.ColorizerGrass;
 import net.minecraft.world.World;
 import net.minecraft.world.biome.Biome;
 import net.minecraft.world.gen.feature.WorldGenAbstractTree;
+import net.minecraft.world.gen.feature.WorldGenBigMushroom;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
 
@@ -21,10 +23,12 @@ public class BiomeRefractedRoofedForest extends Biome implements IBiomeMist {
     protected static final WorldGenAbstractTree TREE = new WorldGenCanopyRefracted();
     protected static final WorldGenAbstractTree TREE2 = new WorldGenEyeTreeDefault();
 
-    protected final float[] lightBrightnessTable = new float[16];
+
+    private float mistDensity = 0.08F;
+    private int mistColor = 4266496;
 
     public BiomeRefractedRoofedForest() {
-        super(new BiomeProperties("Refracted Roofed Forest").setTemperature(3.0F).setRainfall(0.8F));
+        super(new BiomeProperties("Refracted Roofed Forest").setTemperature(0.3F).setRainfall(0.8F));
 
         this.decorator.treesPerChunk = 8;
         this.spawnableCaveCreatureList.clear();
@@ -60,6 +64,48 @@ public class BiomeRefractedRoofedForest extends Biome implements IBiomeMist {
         return ((ColorizerFoliage.getFoliageColor(var1, var3) & 0xFEFEFE) + 0x1E0E4E) / 2;
     }
 
+    public void addRoofedTrees(World p_185379_1_, Random p_185379_2_, BlockPos p_185379_3_) {
+
+        for (int i = 0; i < 4; ++i) {
+            for (int j = 0; j < 4; ++j) {
+                int k = i * 4 + 1 + 8 + p_185379_2_.nextInt(3);
+                int l = j * 4 + 1 + 8 + p_185379_2_.nextInt(3);
+                BlockPos blockpos = p_185379_1_.getHeight(p_185379_3_.add(k, 0, l));
+
+                if (p_185379_2_.nextInt(20) == 0 && net.minecraftforge.event.terraingen.TerrainGen.decorate(p_185379_1_, p_185379_2_, new net.minecraft.util.math.ChunkPos(p_185379_3_), blockpos, net.minecraftforge.event.terraingen.DecorateBiomeEvent.Decorate.EventType.BIG_SHROOM)) {
+                    WorldGenAbstractTree worldgenbigmushroom = new WorldGenCanopyRefracted();
+                    worldgenbigmushroom.generate(p_185379_1_, p_185379_2_, blockpos);
+                } else {
+                    addWorldgenBase(p_185379_1_, p_185379_2_, p_185379_3_, blockpos);
+                }
+            }
+        }
+    }
+
+    private void addWorldgenBase(World p_185379_1_, Random p_185379_2_, BlockPos p_185379_3_, BlockPos blockpos) {
+        if (net.minecraftforge.event.terraingen.TerrainGen.decorate(p_185379_1_, p_185379_2_, new net.minecraft.util.math.ChunkPos(p_185379_3_), blockpos, net.minecraftforge.event.terraingen.DecorateBiomeEvent.Decorate.EventType.TREE)) {
+            WorldGenAbstractTree worldgenabstracttree = this.getRandomTreeFeature(p_185379_2_);
+            worldgenabstracttree.setDecorationDefaults();
+
+            if (worldgenabstracttree.generate(p_185379_1_, p_185379_2_, blockpos)) {
+                worldgenabstracttree.generateSaplings(p_185379_1_, p_185379_2_, blockpos);
+            }
+        }
+    }
+
+    @Override
+    public void decorate(World worldIn, Random rand, BlockPos pos) {
+        this.addMushrooms(worldIn, rand, pos);
+        this.addRoofedTrees(worldIn, rand, pos);
+
+        if (net.minecraftforge.event.terraingen.TerrainGen.decorate(worldIn, rand, new net.minecraft.util.math.ChunkPos(pos), net.minecraftforge.event.terraingen.DecorateBiomeEvent.Decorate.EventType.FLOWERS)) { // no tab for patch
+            int i = rand.nextInt(5) - 3;
+            i += 2;
+            this.addDoublePlants(worldIn, rand, pos, i);
+        }
+        super.decorate(worldIn, rand, pos);
+    }
+
     public void addMushrooms(World p_185379_1_, Random p_185379_2_, BlockPos p_185379_3_) {
 
         for (int i = 0; i < 4; ++i) {
@@ -69,27 +115,48 @@ public class BiomeRefractedRoofedForest extends Biome implements IBiomeMist {
                 BlockPos blockpos = p_185379_1_.getHeight(p_185379_3_.add(k, 0, l));
 
                 if (p_185379_2_.nextInt(20) == 0 && net.minecraftforge.event.terraingen.TerrainGen.decorate(p_185379_1_, p_185379_2_, new net.minecraft.util.math.ChunkPos(p_185379_3_), blockpos, net.minecraftforge.event.terraingen.DecorateBiomeEvent.Decorate.EventType.BIG_SHROOM)) {
-                    WorldGenAbstractTree worldgenbigmushroom = new WorldGenEyeTreeDefault();
+                    WorldGenBigMushroom worldgenbigmushroom = new WorldGenBigMushroom();
                     worldgenbigmushroom.generate(p_185379_1_, p_185379_2_, blockpos);
-                } else if (net.minecraftforge.event.terraingen.TerrainGen.decorate(p_185379_1_, p_185379_2_, new net.minecraft.util.math.ChunkPos(p_185379_3_), blockpos, net.minecraftforge.event.terraingen.DecorateBiomeEvent.Decorate.EventType.TREE)) {
-                    WorldGenAbstractTree worldgenabstracttree = this.getRandomTreeFeature(p_185379_2_);
-                    worldgenabstracttree.setDecorationDefaults();
+                } else addWorldgenBase(p_185379_1_, p_185379_2_, p_185379_3_, blockpos);
+            }
+        }
+    }
 
-                    if (worldgenabstracttree.generate(p_185379_1_, p_185379_2_, blockpos)) {
-                        worldgenabstracttree.generateSaplings(p_185379_1_, p_185379_2_, blockpos);
-                    }
+    public void addDoublePlants(World p_185378_1_, Random p_185378_2_, BlockPos p_185378_3_, int p_185378_4_) {
+        for (int i = 0; i < p_185378_4_; ++i) {
+            int j = p_185378_2_.nextInt(3);
+
+            if (j == 0) {
+                DOUBLE_PLANT_GENERATOR.setPlantType(BlockDoublePlant.EnumPlantType.SYRINGA);
+            } else if (j == 1) {
+                DOUBLE_PLANT_GENERATOR.setPlantType(BlockDoublePlant.EnumPlantType.ROSE);
+            } else if (j == 2) {
+                DOUBLE_PLANT_GENERATOR.setPlantType(BlockDoublePlant.EnumPlantType.PAEONIA);
+            }
+
+            for (int k = 0; k < 5; ++k) {
+                int l = p_185378_2_.nextInt(16) + 8;
+                int i1 = p_185378_2_.nextInt(16) + 8;
+                int j1 = p_185378_2_.nextInt(p_185378_1_.getHeight(p_185378_3_.add(l, 0, i1)).getY() + 32);
+
+                if (DOUBLE_PLANT_GENERATOR.generate(p_185378_1_, p_185378_2_, new BlockPos(p_185378_3_.getX() + l, j1, p_185378_3_.getZ() + i1))) {
+                    break;
                 }
             }
         }
     }
 
-    public void decorate(World worldIn, Random rand, BlockPos pos) {
-        this.addMushrooms(worldIn, rand, pos);
+    public float getMistDensity() {
+        return mistDensity;
+    }
+
+    public void setMistDensity(float mistDensity) {
+        this.mistDensity = mistDensity;
     }
 
     @Override
     public float getMistDensity(int var1, int var2, int var3) {
-        return 0.08F;
+        return mistDensity;
     }
 
     public int getMistColor() {
@@ -100,7 +167,6 @@ public class BiomeRefractedRoofedForest extends Biome implements IBiomeMist {
         this.mistColor = mistColor;
     }
 
-    private int mistColor = 4266496;
     @Override
     public int getMistColour(int var1, int var2, int var3) {
         return mistColor;
--- src/main/java/com/smellysox345/TheShatteredWorld/World/gen/structures/WorldGenCustomStructures.java
+++ src/main/java/com/smellysox345/TheShatteredWorld/World/gen/structures/WorldGenCustomStructures.java
@@ -1,21 +1,11 @@
 package com.smellysox345.TheShatteredWorld.World.gen.structures;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Random;
-
+import com.smellysox345.TheShatteredWorld.World.Biomes.BiomeRefractedRoofedForest;
 import com.smellysox345.TheShatteredWorld.World.Biomes.BiomeShroomValley;
 import com.smellysox345.TheShatteredWorld.World.gen.generators.WorldGenStructure;
-import com.smellysox345.TheShatteredWorld.World.gen.generators.shroomGens.ShroomPatchG;
-import com.smellysox345.TheShatteredWorld.World.gen.generators.shroomGens.ShroomPatchN;
-import com.smellysox345.TheShatteredWorld.World.gen.generators.shroomGens.ShroomPatchP;
-import com.smellysox345.TheShatteredWorld.World.gen.generators.shroomGens.ShroomPatchR;
-import com.smellysox345.TheShatteredWorld.World.gen.generators.shroomGens.GrassShroom;
-import com.smellysox345.TheShatteredWorld.World.gen.generators.shroomGens.PinkiShroom;
-import com.smellysox345.TheShatteredWorld.World.gen.generators.shroomGens.RefractaShroom;
+import com.smellysox345.TheShatteredWorld.World.gen.generators.shroomGens.*;
 import com.smellysox345.TheShatteredWorld.init.BiomeInit;
 import com.smellysox345.TheShatteredWorld.init.ModBlocks;
-
 import net.minecraft.block.Block;
 import net.minecraft.init.Blocks;
 import net.minecraft.util.math.BlockPos;
@@ -25,234 +15,183 @@ import net.minecraft.world.biome.Biome;
 import net.minecraft.world.chunk.IChunkProvider;
 import net.minecraft.world.gen.IChunkGenerator;
 import net.minecraft.world.gen.feature.WorldGenerator;
-import net.minecraftforge.common.BiomeManager.BiomeType;
 import net.minecraftforge.fml.common.IWorldGenerator;
 
-public class WorldGenCustomStructures implements IWorldGenerator{
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Random;
+
+public class WorldGenCustomStructures implements IWorldGenerator {
 
 
-	public static final WorldGenStructure SHROOM_BIG_R = new WorldGenStructure("shroom_r_n");
-	
-	@Override
-	public void generate(Random random, int chunkX, int chunkZ, World world, IChunkGenerator chunkGenerator, IChunkProvider chunkProvider) 
-	{
-		switch(world.provider.getDimension())
-		{
-		case 123321:
-			
-			break;
-			
-		case 1:
-			
-			break;
-			
-		case 0:
-			
-	            generateSurface(world, random, chunkX * 16, chunkZ * 16, chunkZ * 16);
-	            generateSurfaceForGShrooms(world, random, chunkX * 16, chunkZ * 16, chunkZ * 16);
-	            generateSurfaceForRShrooms(world, random, chunkX * 16, chunkZ * 16, chunkZ * 16);
-	            generateSurfaceForPShrooms(world, random, chunkX * 16, chunkZ * 16, chunkZ * 16);
-	            generateSurfaceForNShrooms(world, random, chunkX * 16, chunkZ * 16, chunkZ * 16);
-	            generateSurfaceForPinkShrooms(world, random, chunkX * 16, chunkZ * 16, chunkZ * 16);
-	            generateSurfaceForGrassShrooms(world, random, chunkX * 16, chunkZ * 16, chunkZ * 16);
-	            generateStructure(SHROOM_BIG_R, world, random, chunkX, chunkZ, 20, ModBlocks.MYCELIUM_REFRACTED_BLOCK, BiomeShroomValley.class);
-	            
-			
-			break;
-			
-		case -1:
-			
-		}
-	}
-	
-	private void generateStructure(WorldGenerator generator, World world, Random random, int chunkX, int chunkZ, int chance, Block topBlock, Class<?>... classes)
-	{
-		ArrayList<Class<?>> classesList = new ArrayList<Class<?>>(Arrays.asList(classes));
-		
-		int x = (chunkX * 16) + random.nextInt(15) + 8;
-		int z = (chunkZ * 16) + random.nextInt(15) + 8;
-		int y = calculateGenerationHeight(world, x, z, topBlock);
-		BlockPos pos = new BlockPos(x,y,z);
-		
-		Class<?> biome = world.provider.getBiomeForCoords(pos).getClass();
-		
-		if(world.getWorldType() != WorldType.FLAT)
-		{
-			if(classesList.contains(biome))
-			{
-				if(random.nextInt(chance) == 0)
-				{
-					generator.generate(world, random, pos);
-				}
-			}
-		}
-	}
-	
-	private void runGenerator(WorldGenerator generator, World world, Random random, int chunkX, int chunkZ, int chance, Block topBlock, Class<?>... classes)
-	{
-		ArrayList<Class<?>> classesList = new ArrayList<Class<?>>(Arrays.asList(classes));
-		
-		int x = (chunkX * 16) + random.nextInt(15) + 8;
-		int z = (chunkZ * 16) + random.nextInt(15) + 8;
-		int y = calculateGenerationHeight(world, x, z, topBlock);
-		BlockPos pos = new BlockPos(x,y,z);
-		
-		Class<?> biome = world.provider.getBiomeForCoords(pos).getClass();
-		
-		if(world.getWorldType() != WorldType.FLAT)
-		{
-			if(classesList.contains(biome))
-			{
-				if(random.nextInt(chance) == 0)
-				{
-					generator.generate(world, random, pos);
-				}
-			}
-		}
-	}
-	
-	private static int calculateGenerationHeight(World world, int x, int z, Block topBlock)
-	{
-		int y = world.getHeight();
-		boolean foundGround = false;
-		
-		while(!foundGround && y-- >= 0)
-		{
-			Block block = world.getBlockState(new BlockPos(x,y,z)).getBlock();
-			foundGround = block == topBlock;
-		}
-		
-		return y;
-	}
-	
-	private void generateSurface(World world, Random rand, int chunkX, int chunkZ, int chunkY) {
+    public static final WorldGenStructure SHROOM_BIG_R = new WorldGenStructure("shroom_r_n");
 
-        WorldGenerator genFire = new RefractaShroom();
-        final int FireChance = 4;
-        
-        
-        if (rand.nextInt(FireChance) <= 1) {
-        	Biome biomeBase = world.getBiome(new BlockPos(chunkX, chunkY, chunkZ));
-    		if (biomeBase == BiomeInit.RF_SHROOM) {
-            int randX = chunkX + rand.nextInt(16);
-            int randZ = chunkZ + rand.nextInt(16);
-            int groundY = getGroundFromAbove(world, randX, randZ);
-            genFire.generate(world, rand, new BlockPos(randX, groundY + 1, randZ));
+    private static int calculateGenerationHeight(World world, int x, int z, Block topBlock) {
+        int y = world.getHeight();
+        boolean foundGround = false;
+
+        while (!foundGround && y-- >= 0) {
+            Block block = world.getBlockState(new BlockPos(x, y, z)).getBlock();
+            foundGround = block == topBlock;
         }
-        }
+
+        return y;
     }
-	
-	private void generateSurfaceForPinkShrooms(World world, Random rand, int chunkX, int chunkZ, int chunkY) {
 
-        WorldGenerator genFire = new PinkiShroom();
-        final int FireChance = 4;
-        
-        
-        if (rand.nextInt(FireChance) <= 1) {
-        	Biome biomeBase = world.getBiome(new BlockPos(chunkX, chunkY, chunkZ));
-    		if (biomeBase == BiomeInit.RF_SHROOM) {
-            int randX = chunkX + rand.nextInt(16);
-            int randZ = chunkZ + rand.nextInt(16);
-            int groundY = getGroundFromAbove(world, randX, randZ);
-            genFire.generate(world, rand, new BlockPos(randX, groundY + 1, randZ));
-        }
-        }
-    }
-	
-	private void generateSurfaceForGrassShrooms(World world, Random rand, int chunkX, int chunkZ, int chunkY) {
-
-        WorldGenerator genFire = new GrassShroom();
-        final int FireChance = 5;
-        
-        
-        if (rand.nextInt(FireChance) <= 1) {
-        	Biome biomeBase = world.getBiome(new BlockPos(chunkX, chunkY, chunkZ));
-    		if (biomeBase == BiomeInit.RF_SHROOM) {
-            int randX = chunkX + rand.nextInt(16);
-            int randZ = chunkZ + rand.nextInt(16);
-            int groundY = getGroundFromAbove(world, randX, randZ);
-            genFire.generate(world, rand, new BlockPos(randX, groundY + 1, randZ));
-        }
-        }
-    }
-	
-	private void generateSurfaceForGShrooms(World world, Random rand, int chunkX, int chunkZ, int chunkY) {
-
-        WorldGenerator genGrassShroomType = new ShroomPatchG();
-        final int Chance = 6;
-        
-        
-        if (rand.nextInt(Chance) <= 1) {
-        	Biome biomeBase = world.getBiome(new BlockPos(chunkX, chunkY, chunkZ));
-    		if (biomeBase == BiomeInit.RF_SHROOM) {
-            int randX = chunkX + rand.nextInt(16);
-            int randZ = chunkZ + rand.nextInt(16);
-            int groundY = getGroundFromAbove(world, randX, randZ);
-            genGrassShroomType.generate(world, rand, new BlockPos(randX, groundY + 1, randZ));
-        }
-        }
-    }
-	
-	private void generateSurfaceForRShrooms(World world, Random rand, int chunkX, int chunkZ, int chunkY) {
-
-        WorldGenerator genRShroomType = new ShroomPatchR();
-        final int Chance = 6;
-        
-        
-        if (rand.nextInt(Chance) <= 1) {
-        	Biome biomeBase = world.getBiome(new BlockPos(chunkX, chunkY, chunkZ));
-    		if (biomeBase == BiomeInit.RF_SHROOM) {
-            int randX = chunkX + rand.nextInt(16);
-            int randZ = chunkZ + rand.nextInt(16);
-            int groundY = getGroundFromAbove(world, randX, randZ);
-            genRShroomType.generate(world, rand, new BlockPos(randX, groundY + 1, randZ));
-        }
-        }
-    }
-	
-	private void generateSurfaceForPShrooms(World world, Random rand, int chunkX, int chunkZ, int chunkY) {
-
-        WorldGenerator genPShroomType = new ShroomPatchP();
-        final int Chance = 5;
-        
-        
-        if (rand.nextInt(Chance) <= 1) {
-        	Biome biomeBase = world.getBiome(new BlockPos(chunkX, chunkY, chunkZ));
-    		if (biomeBase == BiomeInit.RF_SHROOM) {
-            int randX = chunkX + rand.nextInt(14);
-            int randZ = chunkZ + rand.nextInt(14);
-            int groundY = getGroundFromAbove(world, randX, randZ);
-            genPShroomType.generate(world, rand, new BlockPos(randX, groundY + 1, randZ));
-        }
-        }
-    }
-	
-	private void generateSurfaceForNShrooms(World world, Random rand, int chunkX, int chunkZ, int chunkY) {
-
-        WorldGenerator genNShroomType = new ShroomPatchN();
-        final int Chance = 6;
-        
-        
-        if (rand.nextInt(Chance) <= 1) {
-        	Biome biomeBase = world.getBiome(new BlockPos(chunkX, chunkY, chunkZ));
-    		if (biomeBase == BiomeInit.RF_SHROOM) {
-            int randX = chunkX + rand.nextInt(16);
-            int randZ = chunkZ + rand.nextInt(16);
-            int groundY = getGroundFromAbove(world, randX, randZ);
-            genNShroomType.generate(world, rand, new BlockPos(randX, groundY + 1, randZ));
-        }
-        }
-    }
-	
-	public static int getGroundFromAbove(World world, int x, int z) {
+    public static int getGroundFromAbove(World world, int x, int z) {
         int y = 255;
         boolean foundGround = false;
         while (!foundGround && y-- >= 0) {
             Block blockAt = world.getBlockState(new BlockPos(x, y, z)).getBlock();
 
             foundGround = blockAt != Blocks.AIR && blockAt != Blocks.WATER && blockAt != Blocks.FLOWING_WATER;
-            
+
         }
         return y;
+    }
+
+    @Override
+    public void generate(Random random, int chunkX, int chunkZ, World world, IChunkGenerator chunkGenerator, IChunkProvider chunkProvider) {
+        BlockPos blockPos = new BlockPos(chunkX * 16, 0, chunkZ * 16);
+        if (world.provider.getBiomeForCoords(blockPos).equals(BiomeInit.R_ROOFED_FOREST)) {
+            generateSurfaceForGrassShrooms(world, random, chunkX * 16, chunkZ * 16, chunkZ * 16);
+            generateSurfaceForRShrooms(world, random, chunkX * 16, chunkZ * 16, chunkZ * 16);
+        }
+        switch (world.provider.getDimension()) {
+            case 123321:
+
+                break;
+
+            case 1:
+
+                break;
+
+            case 0:
+
+                generateSurface(world, random, chunkX * 16, chunkZ * 16, chunkZ * 16);
+                generateSurfaceForGShrooms(world, random, chunkX * 16, chunkZ * 16, chunkZ * 16);
+                generateSurfaceForRShrooms(world, random, chunkX * 16, chunkZ * 16, chunkZ * 16);
+                generateSurfaceForPShrooms(world, random, chunkX * 16, chunkZ * 16, chunkZ * 16);
+                generateSurfaceForNShrooms(world, random, chunkX * 16, chunkZ * 16, chunkZ * 16);
+                generateSurfaceForPinkShrooms(world, random, chunkX * 16, chunkZ * 16, chunkZ * 16);
+                generateSurfaceForGrassShrooms(world, random, chunkX * 16, chunkZ * 16, chunkZ * 16);
+                generateStructure(SHROOM_BIG_R, world, random, chunkX, chunkZ, 20, ModBlocks.MYCELIUM_REFRACTED_BLOCK, BiomeShroomValley.class);
+
+
+                break;
+
+            case -1:
+
+        }
+    }
+
+    private void generateStructure(WorldGenerator generator, World world, Random random, int chunkX, int chunkZ, int chance, Block topBlock, Class<?>... classes) {
+        baseStructureGen(generator, world, random, chunkX, chunkZ, chance, topBlock, classes);
+    }
+
+    private void baseStructureGen(WorldGenerator generator, World world, Random random, int chunkX, int chunkZ, int chance, Block topBlock, Class<?>[] classes) {
+        ArrayList<Class<?>> classesList = new ArrayList<Class<?>>(Arrays.asList(classes));
+
+        int x = (chunkX * 16) + random.nextInt(15) + 8;
+        int z = (chunkZ * 16) + random.nextInt(15) + 8;
+        int y = calculateGenerationHeight(world, x, z, topBlock);
+        BlockPos pos = new BlockPos(x, y, z);
+
+        Class<?> biome = world.provider.getBiomeForCoords(pos).getClass();
+
+        if (world.getWorldType() != WorldType.FLAT) {
+            if (classesList.contains(biome)) {
+                if (random.nextInt(chance) == 0) {
+                    generator.generate(world, random, pos);
+                }
+            }
+        }
+    }
+
+    private void runGenerator(WorldGenerator generator, World world, Random random, int chunkX, int chunkZ, int chance, Block topBlock, Class<?>... classes) {
+        baseStructureGen(generator, world, random, chunkX, chunkZ, chance, topBlock, classes);
+    }
+
+    private void generateSurface(World world, Random rand, int chunkX, int chunkZ, int chunkY) {
+
+        WorldGenerator genFire = new RefractaShroom();
+        final int FireChance = 4;
+
+
+        genBase2(world, rand, chunkX, chunkZ, chunkY, genFire, FireChance);
+    }
+
+    private void genBase2(World world, Random rand, int chunkX, int chunkZ, int chunkY, WorldGenerator genFire, int fireChance) {
+        if (rand.nextInt(fireChance) <= 1) {
+            Biome biomeBase = world.getBiome(new BlockPos(chunkX, chunkY, chunkZ));
+            if (biomeBase == BiomeInit.RF_SHROOM) {
+                int randX = chunkX + rand.nextInt(16);
+                int randZ = chunkZ + rand.nextInt(16);
+                int groundY = getGroundFromAbove(world, randX, randZ);
+                genFire.generate(world, rand, new BlockPos(randX, groundY + 1, randZ));
+            }
+        }
+    }
+
+    private void generateSurfaceForPinkShrooms(World world, Random rand, int chunkX, int chunkZ, int chunkY) {
+
+        WorldGenerator genFire = new PinkiShroom();
+        final int FireChance = 4;
+
+
+        genBase2(world, rand, chunkX, chunkZ, chunkY, genFire, FireChance);
+    }
+
+    private void generateSurfaceForGrassShrooms(World world, Random rand, int chunkX, int chunkZ, int chunkY) {
+
+        WorldGenerator genFire = new GrassShroom();
+        final int FireChance = 5;
+
+
+        genBase2(world, rand, chunkX, chunkZ, chunkY, genFire, FireChance);
+    }
+
+    private void generateSurfaceForGShrooms(World world, Random rand, int chunkX, int chunkZ, int chunkY) {
+
+        WorldGenerator genGrassShroomType = new ShroomPatchG();
+        final int Chance = 6;
+
+
+        genBase2(world, rand, chunkX, chunkZ, chunkY, genGrassShroomType, Chance);
+    }
+
+    private void generateSurfaceForRShrooms(World world, Random rand, int chunkX, int chunkZ, int chunkY) {
+
+        WorldGenerator genRShroomType = new ShroomPatchR();
+        final int Chance = 6;
+
+
+        genBase2(world, rand, chunkX, chunkZ, chunkY, genRShroomType, Chance);
+    }
+
+    private void generateSurfaceForPShrooms(World world, Random rand, int chunkX, int chunkZ, int chunkY) {
+
+        WorldGenerator genPShroomType = new ShroomPatchP();
+        final int Chance = 5;
+
+
+        if (rand.nextInt(Chance) <= 1) {
+            Biome biomeBase = world.getBiome(new BlockPos(chunkX, chunkY, chunkZ));
+            if (biomeBase == BiomeInit.RF_SHROOM) {
+                int randX = chunkX + rand.nextInt(14);
+                int randZ = chunkZ + rand.nextInt(14);
+                int groundY = getGroundFromAbove(world, randX, randZ);
+                genPShroomType.generate(world, rand, new BlockPos(randX, groundY + 1, randZ));
+            }
+        }
+    }
+
+    private void generateSurfaceForNShrooms(World world, Random rand, int chunkX, int chunkZ, int chunkY) {
+
+        WorldGenerator genNShroomType = new ShroomPatchN();
+        final int Chance = 6;
+
+
+        genBase2(world, rand, chunkX, chunkZ, chunkY, genNShroomType, Chance);
     }
 }

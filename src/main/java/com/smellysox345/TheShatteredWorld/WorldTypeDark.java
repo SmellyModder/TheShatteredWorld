104df1d089c8ef3019d17022ad411fd2b70599b4
TheSmileyMan <jesusperalta1026@gmail.com>
2018-09-01 23:59:06 -0600

    Added Nagrot Advancement

--- src/main/java/com/smellysox345/TheShatteredWorld/WorldTypeDark.java
+++ src/main/java/com/smellysox345/TheShatteredWorld/WorldTypeDark.java
@@ -6,19 +6,19 @@ import net.minecraft.world.WorldType;
 import net.minecraft.world.biome.BiomeProvider;
 import net.minecraft.world.biome.BiomeProviderSingle;
 
-public class WorldTypeDark extends WorldType {
+public class WorldTypeDark extends WorldType
+{
     public void init() {
     }
+
     public WorldTypeDark() {
         super("Roofed Forest");
 
     }
 
     @Override
-    public BiomeProvider getBiomeProvider(World world){
-
-
+    public BiomeProvider getBiomeProvider(World world)
+    {
         return new BiomeProviderSingle(BiomeInit.R_ROOFED_FOREST);
-
     }
 }
--- src/main/java/com/smellysox345/TheShatteredWorld/proxy/ClientProxy.java
+++ src/main/java/com/smellysox345/TheShatteredWorld/proxy/ClientProxy.java
@@ -14,8 +14,6 @@ import net.minecraft.client.renderer.block.model.ModelResourceLocation;
 
 public class ClientProxy extends CommonProxy 
 {
-
-	
 	@Override
 	public void preInit(FMLPreInitializationEvent event)
 	{
@@ -35,11 +33,10 @@ public class ClientProxy extends CommonProxy
 	}
 	
 	
-	public void registerItemRenderer(Item item, int meta, String id) {
-			
-			ModelLoader.setCustomModelResourceLocation(item, meta, new ModelResourceLocation(item.getRegistryName(), id));
-		}
-	
+	public void registerItemRenderer(Item item, int meta, String id)
+	{
+		ModelLoader.setCustomModelResourceLocation(item, meta, new ModelResourceLocation(item.getRegistryName(), id));
+	}
 	
 	@Override
 	public void registerVariantRenderer(Item item, int meta, String filename, String id) 
